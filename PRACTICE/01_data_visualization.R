56cb291c726a561977d498fff58d171d50313970
paulogermano2025 <paulo.scherer@ufpr.br>
2025-09-05 21:19:03 -0300

    Primeira adição. Criação dos documentos: 01_data_visualization.R book_study.Rmd

---
+++ PRACTICE/01_data_visualization.R
@@ -0,0 +1,5 @@
+library(tidyverse) # carregar biblioteca contendo package ggplot2
+library(palmerpenguins) # carregar pacote com dataset
+library(ggthemes) # temas gráficos com paleta adaptada para daltônicos
+
+print(penguins, n = 50) # exibir n linhas do conjunto de dados 'penguins'
