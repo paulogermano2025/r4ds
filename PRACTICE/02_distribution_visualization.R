0bf38e06b9fcfe37a89a65a3bb4bc5b399531869
paulogermano2025 <paulo.scherer@ufpr.br>
2025-09-08 21:21:29 -0300

    02_distribution_visualization.R atualizado com testes de violin plot.

--- PRACTICE/02_distribution_visualization.R
+++ PRACTICE/02_distribution_visualization.R
@@ -3,8 +3,23 @@
 library(tidyverse)
 library(palmerpenguins)
 library(ggthemes)
+library(vioplot)
 
 ## Criando gráfico de barras verticais frequência de ocorrência de cada espécie
 
 ggplot(penguins, aes(x = species)) +
   geom_bar(aes(colour = 'tomato'))
+
+
+vioplot(penguins$bill_depth_mm, grp,
+        col = 'tomato')
+
+vioplot(bill_length_mm ~ island , data = penguins,
+        main = 'what?',
+        xlab = 'Island', ylab = 'Bill Profundo',
+        col = c('lightgreen', 'lightblue', 'palevioletred'))
+        legend('right', legend = c('Biscoe','Dream','Torgersen'),
+               fill = c('lightgreen', 'lightblue', 'palevioletred'), cex = 0.7)
+
+?vioplot
+
